1a03a755cb810e80b5cf4211a0ef7307928a237d
Thuwayba15 <thuwaybadawood15@gmail.com>
2026-03-26 14:06:04 +0200

    [feat]: implement approval/rejection of clinicians and apply migrations accordingly

--- backend/aspnet-core/src/MedStream.EntityFrameworkCore/Migrations/20260326103329_AddFacilityGovernanceAndClinicianDecisionAudit.Designer.cs
+++ backend/aspnet-core/src/MedStream.EntityFrameworkCore/Migrations/20260326103329_AddFacilityGovernanceAndClinicianDecisionAudit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MedStream.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MedStream.Migrations
 {
     [DbContext(typeof(MedStreamDbContext))]
-    partial class MedStreamDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260326103329_AddFacilityGovernanceAndClinicianDecisionAudit")]
+    partial class AddFacilityGovernanceAndClinicianDecisionAudit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1490,9 +1493,6 @@ namespace MedStream.Migrations
 
                     b.Property<long?>("ClinicianDeclinedByUserId")
                         .HasColumnType("bigint");
-
-                    b.Property<int?>("ClinicianFacilityId")
-                        .HasColumnType("integer");
 
                     b.Property<DateTime?>("ClinicianSubmittedAt")
                         .HasColumnType("timestamp with time zone");
